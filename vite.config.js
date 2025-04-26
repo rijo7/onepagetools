9fe95b683170e12bf083b29fe0a2d64020c8fd9c
rijo7 <rijov929@gmail.com>
2025-04-26 16:30:19 +0530

    Fix Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/onepagetools/',   // important for GitHub Pages
   plugins: [react()],
-  base: '/onepagetools/',  // important for GitHub Pages
 })
